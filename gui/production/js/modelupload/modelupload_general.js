95bd5a01a933302299295694a0a269b89b96b98c
kevin <kevin.riedl.privat@gmail.com>
2018-03-11 18:56:53 +0100

    rvm logs

--- gui/production/js/modelupload/modelupload_general.js
+++ gui/production/js/modelupload/modelupload_general.js
@@ -1,6 +1,7 @@
 /** Prints general table row to tbody elem from json str */
 function printModelTableRow(tbodyidentifier,jsonObj) {
     /** Extract values from parsed json obj (extracted here, to make method more readable)*/
+    var objectTripleUUID = jsonObj["objectTripleID"];
     var description = jsonObj["description"];
     var creator = jsonObj["creator"];
     var owner = jsonObj["owner"];
@@ -8,21 +9,22 @@ function printModelTableRow(tbodyidentifier,jsonObj) {
     var fileSize = jsonObj["fileSize"];
 
     var compressionTypesList = "";
-    console.log("JsonObjFiles length: "+Object.keys(jsonObj["files"]).length);
+    //console.log("JsonObjFiles length: "+Object.keys(jsonObj["files"]).length);
     for (var index = 0; index < Object.keys(jsonObj["files"]).length; ++index) {
         //TODO: place here relevant ata to string
         compressionTypesList += "<li><img src='images/user.png' class='avatar' alt='Avatar' data-toggle='tooltip' data-placement='top' title='Your compression title'></li>";
-        console.log("Added string to types list. ");
+        //console.log("Added string to types list. ");
     }
 
-    var printFunction = $(tbodyidentifier).html("<tr><td>#</td><td><a>"+description+"</a><br/>" +
+    //objectTripleUUID as Row ID, so we can get id as reference when selecting an action btn
+    var printFunction = $(tbodyidentifier).html("<tr id='"+objectTripleUUID+"'><td>#</td><td><a>"+description+"</a><br/>" +
         "<small data-toggle='tooltip' data-placement='top' title='Owner: "+owner+" / Creator: "+creator+"'>Created on "+createDate+"</small>" +
         "</td><td><ul class='list-inline'>"+compressionTypesList+"</ul></td>" +
         "<td><button type='button' class='btn btn-success btn-xs'>Success</button></td>" +
         "<td><a href='#' class='btn btn-primary btn-xs'><i class='fa fa-folder'></i> View </a>" +
         "<a href='#' class='btn btn-info btn-xs'><i class='fa fa-pencil'></i> Add</a>" +
         "<a href='#' class='btn btn-danger btn-xs'><i class='fa fa-trash-o'></i> Delete </a></td></tr>").fadeIn('slow');
-    console.log("Tried to execute printModelTableRow(): "+tbodyidentifier+";;"+JSON.stringify(jsonObj));
+    //console.log("Tried to execute printModelTableRow(): "+tbodyidentifier+";;"+JSON.stringify(jsonObj));
 }
 
 /** Prints search results so just give all JsonObjs as an Array to this method :)*/
@@ -30,7 +32,7 @@ function printAllModelTableRows(tbodyidentifier, jsonObjs) {
     if (jsonObjs !== null) {
         if (Array.isArray(jsonObjs)) {
             for (var i = 0;i<jsonObjs.length;i++) {
-                console.log('Trying to print table row of provided array->'+JSON.stringify(jsonObjs[i]));
+                //console.log('Trying to print table row of provided array->'+JSON.stringify(jsonObjs[i]));
                 printModelTableRow(tbodyidentifier,jsonObjs[i]);
             }
         } else {
@@ -50,7 +52,7 @@ function printTestJsons(tbodyidentifier, jsonStr) {
     //Method: Extract important values from jsonStr and set it to tableRow template below
     var testResultSet = []; //Instead of this array the search results should be put in
     if (jsonStr !== "") {
-        console.log("Tried to parse provided json str.");
+        //console.log("Tried to parse provided json str.");
         testResultSet.push(JSON.parse(jsonStr)); //add test json to print
     } else {
         //use default (just for testing)
