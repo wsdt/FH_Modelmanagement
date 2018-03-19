0167baa543937b90c9415cdbdfd88ac75ae65792
kevin <kevin.riedl.privat@gmail.com>
2018-03-19 12:50:42 +0100

    js constructors changed, now with setters

--- gui/production/js/modelupload/modelObj.js
+++ gui/production/js/modelupload/modelObj.js
@@ -22,70 +22,157 @@ function isJsonParamValid(json) {
     return jsonObj;
 }
 
-/** Constructor
- * TODO: SETTER for configuring them */
-function ModelObj(json) {
+/** Constructors -------------------------------------------- */
+var ModelObj = function(json) {
     var jsonObj = isJsonParamValid(json); //might also contain false
     if (jsonObj !== false && jsonObj !== null && jsonObj !== undefined) {
-        this.getDescription = function () {
-            return jsonObj.description;
-        };
-        this.getObjectTripleID = function () {
-            return jsonObj.objectTripleID;
-        };
-        this.getMediaTripleID = function () {
-            return jsonObj.mediaTripleID;
-        };
-        this.getCreateDate = function () {
-            return jsonObj.createDate;
-        };
-        this.getCreator = function () {
-            return jsonObj.creator;
-        };
-        this.getOwner = function () {
-            return jsonObj.owner;
-        };
-        this.getMIMEType = function () {
-            return jsonObj.MIMEtype;
-        };
-        this.getFiles = function () {
-            var compressionUUIDs = Object.keys(jsonObj);
-            var compressionObjs = {};
-            for (var i = 0;i<compressionUUIDs.length;i++) {
-                compressionObjs.push(new CompressionObj(compressionUUIDs[i],jsonObj.files[compressionUUIDs[i]]));
-            }
-            return compressionObjs;
-        };
-        this.getFile = function (compressionUUID) {
-            return new CompressionObj(compressionUUID, jsonObj.files[compressionUUID]);
-        }
-    } //no else necessary bc. last statement (return empty obj)
-}
+        Object.defineProperties(this,{
+            "description": {
+                "get": function () {
+                    return this.description;
+                },
+                "set": function (description) {
+                    this.description = description; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "objectTripleID": {
+                "get": function () {
+                    return this.objectTripleID;
+                },
+                "set": function (objectTripleID) {
+                    this.objectTripleID = objectTripleID; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "mediaTripleID": {
+                "get": function () {
+                    return this.mediaTripleID;
+                },
+                "set": function (mediaTripleID) {
+                    this.mediaTripleID = mediaTripleID; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "createDate": {
+                "get": function () {
+                    return this.createDate;
+                },
+                "set": function (createDate) {
+                    this.createDate = createDate; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "creator": {
+                "get": function () {
+                    return this.creator;
+                },
+                "set": function (creator) {
+                    this.creator = creator; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "owner": {
+                "get": function () {
+                    return this.owner;
+                },
+                "set": function (owner) {
+                    this.owner = owner; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "MIMEtype": {
+                "get": function () {
+                    return this.MIMEtype;
+                },
+                "set": function (MIMEtype) {
+                    this.MIMEtype = MIMEtype; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "files": {
+                "get": function () {
+                    return this.files;
+                },
+                "set": function (files) {
+                    var filesObj;
+                    if (files.constructor === {}.constructor) {
+                        filesObj = files;
+                    } else if (files.constructor === "string".constructor) {
+                        filesObj = JSON.parse(files);
+                    } else {
+                        console.warn('modelObj:constructor: Could not parse filesObj. ');
+                        return;
+                    }
 
-//TODO: With setters
-function CompressionObj(compressionUUID,json) {
+                    var compressionUUIDs = Object.keys(filesObj);
+                    var compressionObjs = {};
+                    for (var i = 0;i<compressionUUIDs.length;i++) {
+                        compressionObjs[compressionUUIDs[i]] = (new CompressionObj(compressionUUIDs[i],jsonObj.files[compressionUUIDs[i]]));
+                    }
+                    this.files = compressionObjs; //as extra param bc. --> id : {provided obj}
+                },
+                "getFile": function (compressionUUID) {
+                    return new CompressionObj(compressionUUID,this.files[compressionUUID]);
+                }
+            }
+        });
+    } //no else necessary bc. last statement (return empty obj)
+};
+
+var CompressionObj = function(compressionUUID,json) {
     var jsonObj = isJsonParamValid(json); //might also contain false
     if (jsonObj !== false && jsonObj !== null && jsonObj !== undefined) {
-        this.getCompressionUUID = function () {
-            return compressionUUID; //as extra param bc. --> id : {provided obj}
-        };
-        this.getUploadDate = function () {
-            return jsonObj.uploadDate;
-        };
-        this.getAccessLevel = function () {
-            return jsonObj.accessLevel;
-        };
-        this.getLicense = function () {
-            return jsonObj.license;
-        };
-        this.getFileSize = function () {
-            return jsonObj.fileSize;
-        };
-        this.getPath = function () {
-            return jsonObj.path;
-        };
-        this.getFileTypeSpecificMeta = function () {
-            return jsonObj.fileTypeSpecificMeta;
-        }
+        Object.defineProperties(this, {
+            "compressionUUID": {
+                "get": function () {
+                    return this.compressionUUID;
+                },
+                "set": function (compressionUUID) {
+                    this.compressionUUID = compressionUUID; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "uploadDate": {
+                "get": function () {
+                    return this.uploadDate;
+                },
+                "set": function (uploadDate) {
+                    this.uploadDate = uploadDate; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "accessLevel": {
+                "get": function () {
+                    return this.accessLevel;
+                },
+                "set": function (accessLevel) {
+                    this.accessLevel = accessLevel; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "license": {
+                "get": function () {
+                    return this.license;
+                },
+                "set": function (license) {
+                    this.license = license; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "fileSize": {
+                "get": function () {
+                    return this.fileSize;
+                },
+                "set": function (fileSize) {
+                    this.fileSize = fileSize; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "path": {
+                "get": function () {
+                    return this.path;
+                },
+                "set": function (path) {
+                    this.path = path; //as extra param bc. --> id : {provided obj}
+                }
+            },
+            "fileTypeSpecificMeta": {
+                "get": function () {
+                    return this.fileTypeSpecificMeta;
+                },
+                "set": function (fileTypeSpecificMeta) {
+                    this.fileTypeSpecificMeta = fileTypeSpecificMeta; //as extra param bc. --> id : {provided obj}
+                }
+            }
+        });
     } //no else necessary bc. last statement (return empty obj)
-}
+};
