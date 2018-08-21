8b8877c407219eb0bef47417bcf8fcf956eb3a59
kevin <kevin.riedl.privat@gmail.com>
2018-08-21 06:06:49 +0200

    moved html and changed some references

--- node_js_migration/backend/controller/routes.js
+++ node_js_migration/backend/controller/routes.js
@@ -1,11 +1,28 @@
-/** Define routes */
+/** Define routes **********************************
+ * Allowed http-methods:
+ * >> post, get, put, delete */
 module.exports = {
     "/": {
-        "get":get_index
+        "get":get_login
+    },
+    "/login": {
+        "get":get_login
     }
 };
 
-function get_index(req, res) {
-    res.send('testget');
+/** Route methods ***********************************/
+const page_dir = "./frontend/html/";
+
+function get_login(req, res) {
+    openFile(page_dir+"login.html",req,res);
+}
+
+function openFile(file, req, res) {
+    const fs = require('fs');
+    fs.readFile(file, "utf8", function(err, data) {
+        if (err) {throw err;} else {
+            res.send(data);
+        }
+    })
 }
 
